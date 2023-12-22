da68795b6075bd795143cfbe4d760c6153c75b22
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-22 21:05:35 +0100

    Fix https

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const API_KEY = "1986480656ec490d950204923202611";
 
 async function getWeatherData(location) {
     try {
-        const url = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}`;
+        const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}`;
         const response = await fetch(url);
         console.log("Response", response);
         if (!response.ok) {
