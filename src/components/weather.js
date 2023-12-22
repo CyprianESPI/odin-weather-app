dd69fe56b27508507ab9d80e12d99cd337144099
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-22 19:33:39 +0100

    Get only curr weather condition icon and text

---
+++ src/components/weather.js
@@ -0,0 +1,8 @@
+class Weather {
+    constructor(condition_text, condition_icon) {
+        this.condition_text = condition_text;
+        this.condition_icon = condition_icon;
+    }
+}
+
+export default Weather;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Weather from "./components/weather";
 import Styles from "./styles.css";
 
 const API_KEY = "1986480656ec490d950204923202611";
@@ -12,9 +13,14 @@ async function getWeatherData(location) {
         }
         const json = await response.json();
         console.log("json", json);
+        console.log("json str", JSON.stringify(json));
+
+        const current_weather = new Weather(json.current.condition.text, json.current.condition.icon);
+        console.log("current_weather:", current_weather);
+        return current_weather;
     } catch (error) {
         console.error("Error", error);
     }
 }
 
-getWeatherData("Brussels");
+const currWeather = getWeatherData("Brussels");
