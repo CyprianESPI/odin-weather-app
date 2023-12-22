877364fa2a5fb05cc867cd5eae97971a157e58de
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-22 23:00:30 +0100

    Handle errors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ async function getWeatherData(location) {
         const response = await fetch(url, {
             mode: "cors", // no-cors, *cors, same-origin);
         });
-        console.log("Response", response);
+        console.log("getWeatherData:", response);
         if (!response.ok) {
             throw new Error("Network response was not OK");
         }
@@ -24,7 +24,7 @@ async function getWeatherData(location) {
         //console.log("current_weather:", current_weather);
         return current_weather;
     } catch (error) {
-        console.error("Error", error);
+        console.error("getWeatherData:", error);
     }
 }
 
@@ -36,7 +36,15 @@ const RESULT = document.getElementById("result");
 const LOCATION = document.getElementById("location");
 
 function updateResult(weather) {
-    console.log(weather);
+    console.log("updateResult:", weather);
+    // Clear DOM if location is empty
+    if (weather.location === "") {
+        MAIN.style.backgroundImage = "";
+        RESULT.innerHTML = "Not found...";
+        return;
+    }
+
+    // Update DOM
     MAIN.style.backgroundImage = `url(https:${weather.condition_icon})`;
     RESULT.innerHTML =
         `<h2>${weather.location}</h2>
@@ -46,6 +54,9 @@ function updateResult(weather) {
 LOCATION.addEventListener('input', (e) => {
     getWeatherData(e.target.value).then(res => {
         updateResult(res);
+    }).catch((e) => {
+        console.error(e);
+        updateResult(new Weather("", "", ""));
     });
 });
 
