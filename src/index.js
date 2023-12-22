9d0ccc71cfe56ddc572b0b44726b118ea181eb49
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-22 21:17:25 +0100

    allow cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const API_KEY = "1986480656ec490d950204923202611";
 async function getWeatherData(location) {
     try {
         const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}`;
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            mode: "cors", // no-cors, *cors, same-origin);
+        });
         console.log("Response", response);
         if (!response.ok) {
             throw new Error("Network response was not OK");
